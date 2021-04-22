e5ef108cd22d24919bbb2c2e96b40e11051e9272
Andrea Rigo <andre.rg96@gmail.com>
2021-04-22 10:01:18 +0200

    Update my_ner

--- my_ner.py
+++ my_ner.py
@@ -1,35 +1,47 @@
 class Sentence:
     ents: list
     
-    def __init__(self, conll_sent: list):
-        self.ents = [NamedEntity(ner) for ner in conll_sent]
-    
-    
-    def __str__(self):
-        return ' '.join([ent.text for ent in self.ents])
+    def __init__(self, conll_sent: list, from_slice=False):
+        if not from_slice:
+            self.ents = [NamedEntity(ner) for ner in conll_sent]
+        else:
+            self.ents = conll_sent
     
     
     def __repr__(self):
-        return str([ent.__repr__() for ent in self.ents])
+        return ' '.join([ent.text for ent in self.ents])
+    
+    
+    def __getitem__(self, slice_):
+        sliced = self.ents[slice_]
+        
+        if not isinstance(sliced, list):
+            sliced = [sliced]
+        
+        return Sentence(sliced, from_slice=True)
+    
+    
+    def __len__(self):
+        return len(self.ents)
 
 
 class NamedEntity:
     text: str
     pos: str
-    chunk_iob: str
-    ent_iob: str
+    chunk_tag: str
+    ent_tag: str
     
     def __init__(self, conll_row):
         elems = conll_row[0].split()
         self.text = elems[0]
         self.pos = elems[1]
-        self.chunk_iob = elems[2]
-        self.ent_iob = elems[3]
-    
-    
-    def __str__(self):
-        return self.text
+        self.chunk_tag = elems[2]
+        self.ent_tag = elems[3]
     
     
     def __repr__(self):
-        return str([self.text, self.pos, self.chunk_iob, self.ent_iob])
+        return self.text
+    
+    
+    def to_list(self):
+        return [self.text, self.pos, self.chunk_tag, self.ent_tag]
